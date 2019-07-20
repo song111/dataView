5dbcb04404daaac8ed62a2dcbcccf77af522670e
songjun <songjun@broada.com>
2019-07-20 10:38:41 +0800

    update: add codemirror

--- src/components/Coder/index.js
+++ src/components/Coder/index.js
@@ -1,18 +1,26 @@
 import { UnControlled as CodeMirror } from 'react-codemirror2'
 import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types';
 import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/material.css'
+// import 'codemirror/theme/material.css'
+import 'codemirror/theme/paraiso-dark.css'
+
 
 class Coder extends PureComponent {
-
+    static propTypes={
+        codeStr:PropTypes.string
+    }
+    constructor(){
+        super()
+    }
     render() {
         return (
             <div className="coder">
                 <CodeMirror
                     value={'{name:"小红"}'}
                     options={{
-                        mode: 'xml',
-                        theme: 'material',
+                        mode: { name: "javascript", json: true },
+                        theme: 'paraiso-dark',
                         lineNumbers: true
                     }}
                     onChange={(editor, data, value) => { }}
@@ -21,5 +29,7 @@ class Coder extends PureComponent {
         )
     }
 }
+
+Coder.default      
 
 export default Coder;
